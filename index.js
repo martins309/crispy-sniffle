1ae003de5ae50c98da2781a3b692907455883f36
Andrew Martins <andrew_omartins@yahoo.com>
2021-02-24 10:24:29 -0500

    I fixed the bug and added radio buttons

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ const defaultState = {
 const action_deposit = 'deposit';
 const action_withdrawal = 'withdrawal';
 
-createDeposit(account, amount => {
+const createDeposit = (account, amount) => {
     return {
-        type: 'deposit',
+        type: action_deposit,
         payload: {
             account,
             amount
         }
     }
-});
+};
 
-createWithdrawal(account, amount => {
+const createWithdrawal = (account, amount) => {
     return {
-        type: 'wtihdrawal',
+        type: action_withdrawal,
         payload: {
             account,
             amount
         }
     }
-});
+};
 
 
 //Reducer thing... 
 
-accounts(state=defaultState, action => {
+const accounts = (state=defaultState, action) => {
     switch(action.type) {
         case action_deposit:
             return {
@@ -46,7 +46,7 @@ accounts(state=defaultState, action => {
                 [action.payload.account]: state[action.payload.account] - action.payload.amount
             }
     }   return state;
-});
+};
 
 const store = createStore( 
     accounts,
@@ -57,6 +57,29 @@ store.subscribe(() => {
     console.log('=== state has updated ===');
     const state = store.getState();
     console.log(state);
+    const checkingbalance = document.querySelector('#checking_balance');
+    const savingsbalance = document.querySelector('#savings_balance');
+    checkingbalance.innerHTML = state.checking;
+    savingsbalance.innerHTML = state.savings;
+});
+
+const incrementButton = document.querySelector('#add');
+const decrementButton = document.querySelector('#subtract');
+const amount = document.querySelector('#amount');
+const selectAccount = document.querySelector('#account_selection');
+
+incrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("amount to increment is", amount.value)
+    console.log(selectAccount.value)
+    const amountValue = parseInt(amount.value)
+    store.dispatch(createDeposit(selectAccount.value, amountValue));
+});
+decrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("amount to decrement", amount.value)
+    const amountValue = parseInt(amount.value)
+    store.dispatch(createWithdrawal(selectAccount.value, amount.value))
 });
 
 
